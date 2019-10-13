9e8d160884d1aa44d94da51d3f58432d374bb24b
Alext96 <alextarberg96@gmail.com>
2019-10-13 22:08:19 +0200

    Create TumblrPictureScraper.py

---
+++ TumblrPictureScraper.py
@@ -0,0 +1,35 @@
+from bs4 import BeautifulSoup
+import requests
+import urllib.request
+import random
+
+x = 2
+
+
+def get_image(x):
+    while x < 5:
+        url = "https://the-memedaddy.tumblr.com/page/" + str(x)
+        x += 1
+        response = requests.get(url)
+        data = response.text
+        soup = BeautifulSoup(data, 'lxml')
+        tags = soup.find_all('img')
+        y = 0
+        for tag in tags:
+            y += 1
+            if y > 2:
+                print(tag.get('src'))
+                src = tag.get('src')
+                download_image(src)
+
+
+def download_image(src):
+    try:
+        name = random.randrange(1, 100000)
+        full_name = str(name) + ".png"
+        urllib.request.urlretrieve(src, full_name)
+    except:
+        pass
+
+
+get_image(x)
